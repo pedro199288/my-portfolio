1455f07bd68c4365623e5d54bddcda4dc2c78f2a
Pedro Monteagudo <pmonteagudo@boxfish.studio>
2020-07-18 19:30:20 +0200

    environment files and config adjustments

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,10 +1,7 @@
 // Exporting the development environment config to use anywhere in the project.
 import { environment } from './../../environments/environment';
-import { environmentProd } from './../../environments/environment.prod';
-import { isDevMode } from '@angular/core';
-
 
 // DevConfig
 export const Config = {
-    API_URL: isDevMode() ? environment.API_URL : environmentProd.API_URL
+    API_URL: environment.API_URL
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-export const environmentProd = {
+export const environment = {
   production: true,
   API_URL: 'https://85.208.22.12:3700/api'
 };
